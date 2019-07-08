1aab13d79c9374a690ac32509845b79d5d6e08f4
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2019-07-08 22:01:18 +0700

    initialized

---
+++ 1.js
@@ -0,0 +1,32 @@
+/*
+  ////////////
+  Tiket Konser
+  ////////////
+
+  Buatlah suatu algoritma untuk pembelian tiket konser.
+
+  [INSTRUCTIONS]
+    1. Peserta konser akan membeli berdasarkan uang yang dia miliki, dan asumsi
+       akan memilih harga yang paling mahal.
+    2. Apabila peserta konser memiliki uang yang lebih rendah dari tiket konser,
+       maka dia akan pulang ke rumah.
+    3. Harga jenis kursi konser, antara lain:
+      - VVIP 1000000
+      - VIP  700000
+      - Reguler 300000
+    4. Apabila membeli tiket, tampilkan dalam format [nama]-[jenis kursi] dan [kembalian]
+
+  [RULES]
+    1. Hanya tulis Algoritma saja
+*/
+
+// type here
+/*
+1. Peserta Konser melihat [uang] yang dimiliki.
+2. Peserta konser melihat [harga].
+3. Jika [uang] yang dimiliki diatas [harga] VVIP, maka dia beli VVIP, bila tidak, dia melihat [harga] berikutnya. 
+4. Jika [uang] yang dimiliki diatas [harga] VIP, maka dia beli VIP, bila tidak, dia melihat [harga] berikutnya. 
+5. Jika [uang] yang dimiliki diatas [harga] Reguler, maka dia beli Reguler, bila tidak, dia pulang kerumah. 
+6. Jika peserta konser membeli tiket, dia memberikan [nama], [jenis kursi], dan [uang]
+7. peserta konser mendapatkan tiket dan tampilan konfirmasi [nama]-[jenis kursi] dan [kembalian].
+*/
